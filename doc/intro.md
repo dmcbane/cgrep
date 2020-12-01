35cabc529aafdff43fa74b19b56291e8f016509c
H. Dale McBane <h.dale.mcbane@gmail.com>
2020-12-01 11:45:32 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cgrep
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/cgrep/core.clj
@@ -0,0 +1,11 @@
+(ns cgrep.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn -main
+  "Entry point for application "
+  []
+  (foo "Why"))
