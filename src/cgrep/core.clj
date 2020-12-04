c3d6eb9b83108ba2885d64bdf15f0301351cb16c
H. Dale McBane <h.dale.mcbane@gmail.com>
2020-12-03 23:08:03 -0500

    Only files

--- src/cgrep/core.clj
+++ src/cgrep/core.clj
@@ -1,24 +1,22 @@
 (ns cgrep.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (let [tmp (str "Hello, " x "!")]
-    (println tmp)
-    tmp))
+;; (defn foo
+;;   "I don't do a whole lot."
+;;   [x]
+;;   (let [tmp (str "Hello, " x "!")]
+;;     (println tmp)
+;;     tmp))
 
 (defn lidar
   "Off just a bit."
-  [dirname]
-  (let [directory (clojure.java.io/file dirname)
-        files (file-seq directory)]
-    (for [f files]
-      (let [ path (.getCanonicalPath f)]
-        (println path)
-        path))))
+  [ dirname ]
+  (let [ directory (clojure.java.io/file dirname)
+         files (file-seq directory) ]
+    (for [ f files :when (.isFile f)]
+      (.getCanonicalPath f))))
 
 (defn -main
-  "Entry point for application "
-  [& args]
-  (for [p args]
-    (lidar p))))
+  "Application Entry Point"
+  [ & args ]
+  (for [ p args ]
+    (lidar p)))
